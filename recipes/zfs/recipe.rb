da1a77f67d98b89492cc9b49bd47c133ea0b3569
SaltwaterC <saltwaterc@gmail.com>
2021-05-05 06:11:40 +0100

    Complete the work on RPM. Needs to be tested as the actual build is done under Ubuntu 20.04.

--- recipes/zfs/recipe.rb
+++ recipes/zfs/recipe.rb
@@ -36,6 +36,9 @@ class Zfs < FPM::Cookery::Recipe
     macros_file = "#{ENV['HOME']}/.rpmmacros"
     File.write macros_file, "%_buildhost mr.staker.ltd\n"
 
+    # for Red Hat and derivatives, this should have happen in Docker, but
+    # ending up with a corrupted rpmdb
+    # https://github.com/openzfs/zfs/issues/7727
     configure '--enable-systemd'
     make '-j1', "#{pkg_type}-utils", "#{pkg_type}-dkms"
 
@@ -58,15 +61,41 @@ class Zfs < FPM::Cookery::Recipe
   end
 
   def install_rpm
-    # needs rebuilding using fpm-shim to add dependencies and bump compression
+    macros_file = "#{ENV['HOME']}/.rpmmacros"
+    File.write macros_file, "%_buildhost mr.staker.ltd\n"
+
     Dir['*.rpm'].each do |pkg_file|
+      abs_pkg_file = File.expand_path pkg_file
       pkg_s = File.basename pkg_file, File.extname(pkg_file)
       next if File.extname(pkg_s) == '.src'
 
       pkg_r = File.basename pkg_s, File.extname(pkg_s)
+      pkg_name = pkg_r[0..-9]
 
-      cp pkg_file, pkgdir("#{pkg_r}.x86_64.rpm")
+      next if pkg_name == 'zfs-test'
+
+      Dir.chdir pkgdir do
+        rm_f pkgdir("#{pkg_r}.x86_64.rpm")
+
+        args = [
+          workdir('fpm-shim').to_s,
+          '--maintainer',
+          build_config[:maintainer],
+          '--input-type',
+          'rpm',
+          '--output-type',
+          'rpm',
+          '--architecture',
+          'x86_64'
+        ]
+
+        args << abs_pkg_file
+
+        sh(*args)
+      end
     end
+
+    rm_f macros_file
   end
 
   # rubocop:disable Metrics/AbcSize
@@ -88,9 +117,9 @@ class Zfs < FPM::Cookery::Recipe
           workdir('fpm-shim').to_s,
           '--maintainer',
           build_config[:maintainer],
-          '--output-type',
-          'deb',
           '--input-type',
+          'deb',
+          '--output-type',
           'deb',
           '--license',
           build_config[:license],
