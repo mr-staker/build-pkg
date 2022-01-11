36fcead7339519b62c2d7ed03cff31717612f230
Ștefan Rusu <saltwaterc@gmail.com>
2022-01-11 02:44:53 +0000

    Fix Rubocop violations for zfs pkg.

--- recipes/zfs/spec/zfs_spec.rb
+++ recipes/zfs/spec/zfs_spec.rb
@@ -6,7 +6,7 @@ describe 'zfs' do
   Dir['/build/pkg/*'].each do |pkg|
     describe file(pkg) do
       # this is a hard limit for CF Pages
-      its(:size) { should < 26214400 }
+      its(:size) { should < 26_214_400 }
     end
   end
 end
