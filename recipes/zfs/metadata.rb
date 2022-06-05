b74d26740a4b6d2d5e58e2339b9e59c19b890e00
SaltwaterC <saltwaterc@gmail.com>
2022-06-05 16:17:56 +0100

    Bump zfs to 2.1.4.

--- recipes/zfs/metadata.rb
+++ recipes/zfs/metadata.rb
@@ -2,8 +2,8 @@
 
 def build_config
   {
-    version: '2.1.3',
-    sha256: 'b61b644547793f409cafd6538a52d78f2f72b0cd013e88340882457c8c9b43fd',
+    version: '2.1.4',
+    sha256: '3b52c0d493f806f638dca87dde809f53861cd318c1ebb0e60daeaa061cf1acf6',
     homepage: 'https://github.com/openzfs/zfs',
     maintainer: 'hello@mr.staker.ltd',
     license: 'CDDL',
